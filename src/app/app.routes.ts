4552d3c951434047c17a7d0d2f5b3d8612487a1e
SirineBETTAIEB <sirinebettaieb30@gmail.com>
2024-05-14 13:08:54 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'sign-up', component: SignUpComponent },
+    {path:'user-profile', component: UserProfileComponent},
+];
